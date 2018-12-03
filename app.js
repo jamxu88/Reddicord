708f511b34e180832c4fd6de370643306dc2117a
James <43918220+jamxu88@users.noreply.github.com>
2018-12-03 11:07:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const Enmap = require("enmap");
 const myEnmap = new Enmap();
-const DB = require("better-sqlite-pool")
+const DB = require("better-sqlite-pool");
 client.karma = new Enmap({name: 'test' });
 client.on("ready", () => {
-  console.log("Rate Bot Online");
+  console.log("Reddicord Activated");
   globalreacts = 323;
-  y = 0;
   if(myEnmap.isReady) { 
   console.log("Data Ready");
   }else {
   console.log("Data Not Loaded");
   }
-  client.user.setActivity("Rate Bot- Front Page Coming Soon!", { type: "STREAMING", url: "https://www.twitch.tv/jamxu" })
+  client.user.setActivity("Rate Bot- Official Launch December 01", { type: "STREAMING", url: "https://www.twitch.tv/jamxu" })
 });
 client.on('messageReactionAdd', (reaction, user, message) => {
   if(reaction.emoji.name === "🔼") {
@@ -41,7 +40,7 @@ client.on('messageReactionAdd', (reaction, user, message) => {
         user: reaction.message.author.id,
         karma: 0
       });
-      client.karma.inc(key, "karma")
+      client.karma.inc(key, "karma");
     }else
       console.log(reaction.message.author.id);
       globalreacts += 1;
@@ -50,7 +49,7 @@ client.on('messageReactionAdd', (reaction, user, message) => {
         user: reaction.message.author.id,
         karma: 0
       });
-      client.karma.dec(key, "karma")
+      client.karma.dec(key, "karma");
   } 
 });
 client.on('messageReactionRemove', (reaction, user, message) => {
@@ -61,7 +60,7 @@ client.on('messageReactionRemove', (reaction, user, message) => {
           user: reaction.message.author.id,
           karma: 0
         });
-        client.karma.inc(key, "karma")
+        client.karma.inc(key, "karma");
       }else
         globalreacts -= 1;
         console.log(reaction.message.author.id);
@@ -70,7 +69,7 @@ client.on('messageReactionRemove', (reaction, user, message) => {
           user: reaction.message.author.id,
           karma: 0
         });
-        client.karma.dec(key, "karma")
+        client.karma.dec(key, "karma");
     }else
     if(reaction.emoji.name === "🔽") {
       if (user.id === reaction.message.author.id) {
@@ -79,7 +78,7 @@ client.on('messageReactionRemove', (reaction, user, message) => {
           user: reaction.message.author.id,
           karma: 0
         });
-        client.karma.dec(key, "karma")
+        client.karma.dec(key, "karma");
       }else
         globalreacts -= 1;
         console.log(reaction.message.author.id);
@@ -88,62 +87,67 @@ client.on('messageReactionRemove', (reaction, user, message) => {
           user: reaction.message.author.id,
           karma: 0
         });
-        client.karma.inc(key, "karma")
+        client.karma.inc(key, "karma");
     }
 });
-const prefix = "^"
+const prefix = "&"
 client.on("message", (message) => {
 function react() {
-  message.react("🔽")
+  message.react("🔽");
   globalreacts = globalreacts + 1
 }
 if(message.author.bot) return;
 if (message.attachments.size > 0) {
     setTimeout(react, 500);
     message.react("🔼");
+}else
+  if (message.content.startsWith("'")) {
+    setTimeout(react,500);
+    message.react("🔼");
   }else
-    if (message.content.startsWith("'")) {
-      setTimeout(react,500);
-      message.react("🔼");
-    }else
-   if (message.content.startsWith(prefix + "ping")) {
-     responsetime = new Date().getTime() - message.createdTimestamp
-     message.channel.send("Pong! There have been "+ globalreacts +" reacts made to date. Response Time: " + responsetime + " ms");
-     console.log("Pinged");
-    }else
-    if (message.content.startsWith(prefix + "creator")) {
-      message.channel.send("This bot was created by jam#3515");
-    }else
-    if (message.content.startsWith(prefix + "github")) {
-      message.channel.send("<https://github.com/jamxu88/Reddicord>");
-    }else
-    if (message.content.startsWith(prefix + "karma")) {
-    const key = `${message.author.id}`;
-    try {
-      message.channel.send(`You currently have ${client.karma.get(key, "karma")} karma!`);
+if (message.content.startsWith(prefix + "ping")) {
+  responsetime = new Date().getTime() - message.createdTimestamp
+  message.channel.send("Pong! There have been "+ globalreacts +" reacts made to date. Response Time: " + responsetime + " ms");
+  console.log("Pinged");
+  }else
+  if (message.content.startsWith(prefix + "creator")) {
+    message.channel.send("This bot was created by jam#3515");
+  }else
+  if (message.content.startsWith(prefix + "github")) {
+    message.channel.send("<https://github.com/jamxu88/Reddicord>");
+  }else
+  if (message.content.startsWith(prefix + "karma")) {
+  const key = `${message.author.id}`;
+  try {
+    message.channel.send(`You currently have ${client.karma.get(key, "karma")} karma!`);
+  }
+  catch(EnmapPathError) {
+    message.channel.send("You currently have no karma!")
+  }
+  }else
+  if (message.content.startsWith(prefix + "leaderboard")) {
+    const filtered = client.karma.array();
+    const sorted = filtered.sort((b, a) => a.karma - b.karma);
+    const top = sorted.splice(0,10);
+    const embed = new Discord.RichEmbed()
+      .setTitle("Karma Leaderboard")
+      .setAuthor(client.user.username, client.user.avatarURL)
+      .setColor(16777215);
+    for(const data of top) {
+      embed.addField(client.users.get(data.user).tag, `${data.karma} karma`);
     }
-    catch(EnmapPathError) {
-      message.channel.send("You have no karma!")
-    }
-    }else
-    if (message.content.startsWith(prefix + "leaderboard")) {
-      const filtered = client.karma.array();
-      const sorted = filtered.sort((b, a) => a.karma - b.karma);
-      const top = sorted.splice(0,10);
-      const embed = new Discord.RichEmbed()
-        .setTitle("Karma Leaderboard")
-        .setAuthor(client.user.username, client.user.avatarURL)
-        .setColor(0x00AE86);
-      for(const data of top) {
-        embed.addField(client.users.get(data.user).tag, `${data.karma} karma`);
-      }
-      return message.channel.send({embed});
-    }else
-    if (message.content.startsWith(prefix + "help")) {
-      message.channel.send({embed: {
-      color: ffffff,
-      description: "**Prefix**: `#` \n **General Commands**: \n `ping`- Ping the bot \n `creator`- Creator of this bot! \n `github`- View this bot's source code \n **Karma Commands**: \n `karma`- Check your karma \n `leaderboard`- Check the top 10 Karma Holders \n How to put Rating on a message: Start message with `'` (single quote)"
-}});
-    }
-  });
-client.login(process.env.BOT_TOKEN);
+    return message.channel.send({embed});
+  }else
+  if (message.content.startsWith(prefix + "help")) {
+    message.channel.send({embed: {
+    color: 16777215,
+    description: "**Prefix**: `"+prefix+"` \n **General Commands**: \n `ping`- Ping the bot \n `creator`- Creator of this bot! \n `github`- View this bot's source code \n \n**Karma Commands**: \n `karma`- Check your karma \n `leaderboard`- Check the top 10 Karma Holders \n \n**How do I get Karma?**- Just post an image, gif, or video in a text channel with reactions enabled, and get some upvotes. Posting a link or joke? Just start your message with `'` (single quote)"
+    }});
+  }else
+  if (message.content.includes('https://')) {
+    setTimeout(react,500);
+    message.react("🔼");
+  }
+});
+//Test Token
+client.login(env.proccess.BOT_TOKEN);
